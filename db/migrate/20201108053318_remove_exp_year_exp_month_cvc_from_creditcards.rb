a8711817f07ffa118be20261fc58d2a6442b399b
noritadaaa <67197096+noritadaaa@users.noreply.github.com>
2020-11-08 16:39:22 +0900

    Revert "不要なカラムの削除"

--- db/migrate/20201108053318_remove_exp_year_exp_month_cvc_from_creditcards.rb
+++
@@ -1,7 +0,0 @@
-class RemoveExpYearExpMonthCvcFromCreditcards < ActiveRecord::Migration[6.0]
-  def change
-    remove_column :creditcards, :exp_year, :string
-    remove_column :creditcards, :exp_month, :string
-    remove_column :creditcards, :cvc, :string
-  end
-end
